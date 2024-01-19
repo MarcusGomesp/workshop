05acf2c5778f53b37a2ff42158d747c4320af540
Marcus Gomes <marcus_1609@hotmail.com>
2024-01-19 10:55:48 -0300

    Primeiro commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using SavesWebMvc.Models;
+using System.Diagnostics;
+
+namespace SavesWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewData["Name"] = "Marcus Vinicius";
+            ViewData["email"] = "marcus@gmail.com";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
